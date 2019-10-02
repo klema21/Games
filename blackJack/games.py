5d6a2503b910730785fcfada651242b8bc3acfd3
IlyaKlementev <klema2727@gmail.com>
2019-10-02 13:22:56 +0300

    Update games.py

--- blackJack/games.py
+++ blackJack/games.py
@@ -1,26 +1,33 @@
 # Games
-# Simple module for future games
+# Demonstrates module creation
 
 class Player(object):
-    """Player"""
+    """ A player for a game. """
     def __init__(self, name, score = 0):
         self.name = name
         self.score = score
+
     def __str__(self):
         rep = self.name + ":\t" + str(self.score)
         return rep
-        
-def ask_yes_or_no(question):
-        response = None
-        while response not in ("y", "n"):
-            response = input(question).lower()
-        return response
+
+def ask_yes_no(question):
+    """Ask a yes or no question."""
+    response = None
+    while response not in ("y", "n"):
+        response = input(question).lower()
+    return response
 
 def ask_number(question, low, high):
-        response = None
-        while response not in range(low, high):
-            response = int(input(question))
-        return response
+    """Ask for a number within a range."""
+    response = None
+    while response not in range(low, high):
+        response = int(input(question))
+    return response
+
+  
 if __name__ == "__main__":
-    print("You started the module 'Games' directly and didn't import it.")
-    input("\nPress Enter key to exit.")
+    print("You ran this module directly (and did not 'import' it).")
+    input("\n\nPress the enter key to exit.")
+
+
