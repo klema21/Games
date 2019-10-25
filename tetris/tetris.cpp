1ac6495c5c2ff6d3fceb12faf2fd616dafb860a7
Ilya Klementev <klema2727@gmail.com>
2019-10-25 19:57:51 +0300

    Add screen

--- tetris/tetris.cpp
+++ tetris/tetris.cpp
@@ -2,6 +2,10 @@
 
 using namespace std;
 
+#include <Windows.h>
+
+int nScreenWidth = 80;
+int nScreenHeight = 30;
 wstring tetromino[7];
 int nFieldWidth = 12;
 int nFieldHeight = 18;
@@ -33,8 +37,7 @@ int Rotate(int px, int py, int r)
 
 int main()
 {
-
-  tetromino[0].append(L"..X...X...X...X."); // Tetronimos 4x4
+    tetromino[0].append(L"..X...X...X...X."); // Tetronimos 4x4
 	tetromino[1].append(L"..X..XX...X.....");
 	tetromino[2].append(L".....XX..XX.....");
 	tetromino[3].append(L"..X..XX..X......");
@@ -42,8 +45,31 @@ int main()
 	tetromino[5].append(L".X...X...XX.....");
 	tetromino[6].append(L"..X...X..XX.....");
 
-  pField = new unsigned char[nFieldWidth*nFieldHeight]; // Create play field buffer
-  for (int x = 0; x < nFieldWidth; x++) // Board Boundary
-    for (int y = 0; y < nFieldHeight; y++)
-      pField[y*nFieldWidth + x] = (x == 0 || x == nFieldWidth-1 || y == nFieldHeight - 1) ? 9 : 0;
+    pField = new unsigned char[nFieldWidth*nFieldHeight]; // Create play field buffer
+    for (int x = 0; x < nFieldWidth; x++) // Board Boundary
+        for (int y = 0; y < nFieldHeight; y++)
+            pField[y*nFieldWidth + x] = (x == 0 || x == nFieldWidth-1 || y == nFieldHeight - 1) ? 9 : 0;
+
+    // Create Screen Buffer
+	wchar_t *screen = new wchar_t[nScreenWidth*nScreenHeight];
+	for (int i = 0; i < nScreenWidth*nScreenHeight; i++) screen[i] = L' ';
+	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	SetConsoleActiveScreenBuffer(hConsole);
+	DWORD dwBytesWritten = 0;
+
+	bool bGameOver = false;
+
+	while (!bGameOver)
+    {
+        // Draw Field
+		for (int x = 0; x < nFieldWidth; x++)
+			for (int y = 0; y < nFieldHeight; y++)
+				screen[(y + 2)*nScreenWidth + (x + 2)] = L" ABCDEFG=#"[pField[y*nFieldWidth + x]];
+
+        // Display Frame
+        WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
+
+    }
+
+    return 0;
 }
