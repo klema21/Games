1649fd7f48825d631429cbbe1e8dc84821ab529f
ilya klementev <klema2727@gmail.com>
2020-11-27 15:58:53 +0300

    Working on a file permission

--- chanceAPI/chanceAPI.h
+++ chanceAPI/chanceAPI.h
@@ -4,13 +4,17 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 
 #define DATAFILE "/var/chance.data" // File to store user data
 
 struct user{
 	int uid;
 	int credits;
-	int hightscore;
+	int highscore;
 	char name[100];
 	//int (*current_game) ();
 };
@@ -22,6 +26,25 @@ int get_player_data();
 void register_new_player();
 void update_player_data();
 void input_name();
+void fatal(char*);
+void *ec_malloc(unsigned int);
+
+void *ec_malloc(unsigned int size){
+	void *ptr;
+	ptr = malloc(size);
+	if(ptr == NULL)
+		fatal("in ec_malloc() on memory allocation");
+   return ptr;	
+}
+
+void fatal(char* message){
+	char error_message[100];
+
+	strcpy(error_message, "[!!] Fatal Error ");
+	strncat(error_message, message, 83);
+	perror(error_message);
+	exit(-1);
+}
 
 void register_new_player(){
 	int fd;
@@ -34,6 +57,13 @@ void register_new_player(){
 	player.highscore = player.credits = 100;
 
 	fd = open(DATAFILE, O_WRONLY|O_CREAT|O_APPEND, S_IRUSR|S_IWUSR);
+	if(fd == -1)
+		fatal("in register_new_player() while opening file");
+	write(fd, &player, sizeof(struct user));
+	close(fd);
+	
+	printf("\nWelcome! %s.\n", player.name);
+	printf("You have been given %u credits\n", player.credits);
 }
 
 void input_name(){
